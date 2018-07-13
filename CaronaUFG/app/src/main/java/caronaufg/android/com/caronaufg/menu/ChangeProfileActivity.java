46408b535d7cdf1b1444ec6a5a6d55dd5e87904f
Jhordan <jhordangabriel@hotmail.com>
2018-07-13 19:02:17 -0300

    carregando email no profile

--- CaronaUFG/app/src/main/java/caronaufg/android/com/caronaufg/menu/ChangeProfileActivity.java
+++ CaronaUFG/app/src/main/java/caronaufg/android/com/caronaufg/menu/ChangeProfileActivity.java
@@ -6,23 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import caronaufg.android.com.caronaufg.R;
 import caronaufg.android.com.caronaufg.home.ProfileOptionActivity;
 
 public class ChangeProfileActivity extends AppCompatActivity {
-
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_profile);
         getSupportActionBar().hide();
         setupChangeProfile();
+        getData();
     }
 
     private void setupChangeProfile() {
-        TextInputEditText changeName = findViewById(R.id.changeNameId);
         TextInputEditText changeEmail = findViewById(R.id.changeEmailId);
         TextInputEditText changeCourse = findViewById(R.id.changeCourseId);
         TextInputEditText currentPass = findViewById(R.id.currentPasswordId);
@@ -31,9 +35,18 @@ public class ChangeProfileActivity extends AppCompatActivity {
         changeSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Alterações salvas", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Alterações salvas com sucesso", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(ChangeProfileActivity.this, ProfileOptionActivity.class));
             }
         });
+    }
+
+    private void getData() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = auth.getCurrentUser();
+
+        String userEmail = (firebaseUser != null) ? firebaseUser.getEmail() : "Convidado";
+        TextView userEmailProfile = findViewById(R.id.changeEmailId);
+        userEmailProfile.setText(userEmail);
     }
 }
